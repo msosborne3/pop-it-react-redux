e5b5d6146a56925835867f1c1a2a0bae2530304f
msosborne3 <msosborne@ymail.com>
2017-04-17 21:27:04 -0500

    Render navbar and children in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from '.NavBar';
+//import MoviesPage from '../containers/MoviesPage';
+// import ShowsPage from '../containers/ShowsPage';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default (props) => {
+  return (
+    <div>
+      <NavBar />
+      {props.children}
+    </div>
+  )
+};
