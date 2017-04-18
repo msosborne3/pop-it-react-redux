63a4c6b23ac4f0eb7379087d4a1dff7f4a6dbe6d
msosborne3 <msosborne@ymail.com>
2017-04-17 22:45:41 -0500

    Add Provider and Routes to index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import MoviesPage from './containers/MoviesPage';
+import ShowsPage from './containers/ShowsPage';
+import ShowsShow from './containers/ShowsShow';
+import MoviesShow from './containers/MoviesShow';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <Route path='/movies' component={MoviesPage}>
+          <Route path='/movies/:id' component={MoviesShow} />
+        </Route>
+        <Route path='/shows' component={ShowsPage}>
+          <Route path='/shows/:id' component={ShowsShow} />
+        </Route>
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
