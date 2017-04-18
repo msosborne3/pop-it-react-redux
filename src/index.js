31a12372a125b7187ba0bd53a848f34c91ac080c
msosborne3 <msosborne@ymail.com>
2017-04-17 23:20:27 -0500

    Create store in index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import App from './components/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
+import { createStore } from 'redux';
+import { rootReducer } from './reducers';
 import MoviesPage from './containers/MoviesPage';
 import ShowsPage from './containers/ShowsPage';
 import ShowsShow from './containers/ShowsShow';
 import MoviesShow from './containers/MoviesShow';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
